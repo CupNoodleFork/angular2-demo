50b4c759309592aba23f79777426875d6e12cdf6
zhangchaowei <zhangchaowei@haizhi.com>
2016-07-07 15:11:10 +0800

    demo

--- app/scripts/app.component.js
+++ app/scripts/app.component.js
@@ -2,12 +2,41 @@
  * Created by haizhi on 16/7/6.
  */
 (function(app) {
+    var HERO = [
+        {
+            'id': 11,
+            'name': '11'
+        },
+        {
+            'id': 12,
+            'name': '12'
+        },
+        {
+            'id': 13,
+            'name': '13'
+        },
+        {
+            'id': 14,
+            'name': '14'
+        },
+        {
+            'id': 15,
+            'name': '15'
+        },
+        {
+            'id': 16,
+            'name': '16'
+        }
+    ];
     app.AppComponent =
         ng.core.Component({
                 selector: 'my-app',
-                template: '<h1>My First Angular 2 App</h1>'
+                template: `
+                    <div>Test</div>
+                `
             })
             .Class({
-                constructor: function() {}
+                constructor: function() {
+                },
             });
 })(window.app || (window.app = {}));
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,9 @@
 /**
  * Created by haizhi on 16/7/6.
  */
+
+((app) => {
+    document.addEventListener('DOMContentLoaded', () => {
+        ng.platformBrowserDynamic.bootstrap(app.AppComponent);
+    })
+})(window.app || (window.app = {}));
